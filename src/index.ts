116eab31e0b12fe12ad8a3de0d39dfa3f7a94861
Elvis Onobo <elvis.onobo@gmail.com>
2022-10-17 18:05:35 +0100

    Initial Setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import 'express-async-errors'
 import app from './app'
-import db from './config/db'
 
 const PORT = 4000
 // connect to database
-db()
 
 app.listen(PORT, () => {
  console.log(`App listening on port ${PORT}`)
